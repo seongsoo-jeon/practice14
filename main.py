92fa4a6eeb964dc668afc619eb5df292fa11398f
seongsoo-jeon <stome159@naver.com>
2024-07-24 11:40:03 +0900

    各選択のプロセスを別のメソッドを作って読みやすくした

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
-#GPA管理システム
-
+# GPA管理システム
 
 GPA = {}
-while True:
+
+def select_option():
     print("1. 成績入力 2. 成績一覧 3.終了")
-    option = int(input("貴方の入力："))
+    opt = int(input("貴方の入力："))
+    return opt
+
+def opt_1st(GPA):
+    sub, gpa = input().split(",")
+    gpa = int(gpa)
+    if sub not in GPA:
+        GPA[sub] = [gpa]
+    else:
+        GPA[sub].append(gpa)
+
+def opt_2nd(GPA):
+    print("ーーーーーーーーーーーーーーーーーーー")
+    for i in GPA:
+        print(i, ":", sum(GPA[i]) / len(GPA[i]))
+    print("ーーーーーーーーーーーーーーーーーーー")
+
+while True:
+
+    option = select_option()
 
     if option == 1:
-        sub, gpa = input().split(",")
-        gpa = int(gpa)
-        if sub not in GPA:
-            GPA[sub] = [gpa]
-        else:
-            GPA[sub].append(gpa)
+        opt_1st(GPA)
 
     elif option == 2:
-        print("ーーーーーーーーーーーーーーーーーーー")
-        for i in GPA:
-            print(i, ":", sum(GPA[i])/len(GPA[i]))
-        print("ーーーーーーーーーーーーーーーーーーー")
+        opt_2nd(GPA)
 
     else:
         break
