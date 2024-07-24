c958f9f2e386a4d7f37d4672b07d076b44c85dca
seongsoo-jeon <stome159@naver.com>
2024-07-24 11:32:44 +0900

    最初に成績を入力するか一覧を見るかを選ベルプログラム２

--- main.py
+++ main.py
@@ -3,13 +3,19 @@
 
 GPA = {}
 while True:
+    print("1. 成績入力 2. 成績一覧")
+    option = int(input("貴方の入力："))
 
-    sub, gpa = input().split(",")
-    gpa = int(gpa)
-    if sub not in GPA:
-        GPA[sub] = [gpa]
+    if option == 1:
+        sub, gpa = input().split(",")
+        gpa = int(gpa)
+        if sub not in GPA:
+            GPA[sub] = [gpa]
+        else:
+            GPA[sub].append(gpa)
+
     else:
-        GPA[sub].append(gpa)
-
-    print(GPA)
-
+        print("ーーーーーーーーーーーーーーーーーーー")
+        for i in GPA:
+            print(i, ":", sum(GPA[i])/len(GPA[i]))
+        print("ーーーーーーーーーーーーーーーーーーー")
