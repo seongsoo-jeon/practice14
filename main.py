4663de9cad4c47716671c35ba7f912fd137408a0
seongsoo-jeon <stome159@naver.com>
2024-07-24 11:24:45 +0900

    科目とGPAをGPAに入れる簡単なプログラム１

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello")
+#GPA管理システム
+
+
+GPA = {}
+while True:
+
+    sub, gpa = input().split(",")
+    gpa = int(gpa)
+    if sub not in GPA:
+        GPA[sub] = [gpa]
+    else:
+        GPA[sub].append(gpa)
+
+    print(GPA)
+
